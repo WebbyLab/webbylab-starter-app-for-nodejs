e8b2b3675948c4b6fc467eea1e5996c20223688e
Vira Rodionova <bepa.rdnv@gmail.com>
2020-03-25 09:48:52 +0200

    Init rrest-api app and global app (models, use-cases base) separately

--- app.mjs
+++ app.mjs
@@ -1,1 +1,26 @@
-import './lib/api/rest-api/app.mjs';
+import { stop as stopRestAPI } from './lib/api/rest-api/app.mjs';
+
+import initModels              from './lib/domain-model/initModels.mjs';
+import UseCaseBase             from './lib/use-cases/Base.mjs';
+import config                  from './lib/config.mjs';
+
+console.log(`[App] Init Mode: ${process.env.MODE}`);
+console.log(`[App] Proccess ID ${process.pid}`);
+
+const dbMode = process.env.MODE === 'application' ? 'db' : 'test-db';
+
+const { sequelize } = initModels(config[dbMode]);
+
+// TODO: change
+UseCaseBase.setSequelizeInstanse(sequelize);
+
+process.on('SIGTERM', async () => {
+    console.log('[App] SIGTERM signal catched');
+
+    await stopRestAPI();
+    console.log('[App] Closing sequelize connections');
+    await sequelize.close();
+
+    console.log('[App] Exit');
+    process.exit(0);
+});
