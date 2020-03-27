e58e0db63d1e7836c8d0e08f137126218731a6ff
Vira Rodionova <bepa.rdnv@gmail.com>
2020-03-27 10:46:47 +0200

    Add default database data insert Add neegative test: NOT_UNIQUE email

--- tests/lib/TestFactory.mjs
+++ tests/lib/TestFactory.mjs
@@ -1,3 +1,5 @@
+import User from '../../lib/domain-model/User.mjs';
+
 class TestFactory {
     constructor() {
 
@@ -8,6 +10,31 @@ class TestFactory {
     }
 
     async setupUsers() {
+        await this._createDefaultUsers();
+    }
+
+    async _createDefaultUsers() {
+        const users = [
+            {
+                email          : 'default1@gmail.com',
+                firstName      : 'First',
+                lastName       : 'Defaulr',
+                password       : 'password',
+                status         : 'PENDING',
+                agreeWithTerms : true
+            },
+            {
+                email          : 'default2@gmail.com',
+                firstName      : 'Second',
+                lastName       : 'Default',
+                password       : 'password',
+                status         : 'ACTIVE',
+                agreeWithTerms : true
+            }
+        ];
+        const savedUsers = await User.bulkCreate(users);
+
+        return savedUsers;
     }
 }
 
