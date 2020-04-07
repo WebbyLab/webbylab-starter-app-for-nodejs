7a00ab42ab2bae4b49def313fea45f5d2e30f678
Vira Rodionova <bepa.rdnv@gmail.com>
2020-04-07 18:37:40 +0300

    Handle SIGINT signal

--- app.mjs
+++ app.mjs
@@ -5,7 +5,6 @@ import UseCaseBase             from './lib/use-cases/Base.mjs';
 import config                  from './lib/config.cjs';
 
 console.log(`[App] Init Mode: ${process.env.MODE}`);
-console.log(`[App] Proccess ID ${process.pid}`);
 
 const dbMode = process.env.MODE === 'application' ? 'db' : 'test-db';
 
@@ -17,10 +16,20 @@ UseCaseBase.setSequelizeInstanse(sequelize);
 process.on('SIGTERM', async () => {
     console.log('[App] SIGTERM signal catched');
 
+    await shutdown();
+});
+
+process.on('SIGINT', async () => {
+    console.log('[App] SIGINT signal catched');
+
+    await shutdown();
+});
+
+async function shutdown() {
     await stopRestAPI();
     console.log('[App] Closing sequelize connections');
     await sequelize.close();
 
     console.log('[App] Exit');
     process.exit(0);
-});
+}
