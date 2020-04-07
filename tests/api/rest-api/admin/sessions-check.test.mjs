6960c512b22fc04eb6302ec7c3b0d715dd6b1162
Vira Rodionova <bepa.rdnv@gmail.com>
2020-04-07 18:57:04 +0300

    Add tests for sessions check by rest api

---
+++ tests/api/rest-api/admin/sessions-check.test.mjs
@@ -0,0 +1,29 @@
+import { getDirName } from '../../../../lib/utils/index.mjs';
+import Tester         from '../Tester.mjs';
+
+const tester = new Tester();
+
+const dirname = getDirName(import.meta.url);
+
+function requestBuilder(token) {
+    return {
+        method  : 'GET',
+        url     : '/api/v1/admin/users',
+        headers : {
+            'Authorization' : token
+        }
+    };
+}
+
+tester.setupTestsWithTransactions(`${dirname}/../../../fixtures/use-cases/admin/sessions-check/negative`,
+    'admin/sessions-check/negative',
+    async ({ config: { before }, input, exception }) => {
+        const tokens = await before(tester.factory);
+        const token = tokens[input.email];
+
+        await tester.testUseCaseNegative({
+            requestBuilder : () => requestBuilder(token),
+            exception
+        });
+    }
+);
