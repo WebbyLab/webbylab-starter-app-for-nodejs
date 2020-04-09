2636a48edc130c4e6ac1d14a935f56450504a92a
Vira Rodionova <bepa.rdnv@gmail.com>
2020-04-09 17:14:43 +0300

    Update StoredTriggerableAction API

--- migrations/20200408132808-StoredTriggerableActions.js
+++ migrations/20200408132808-StoredTriggerableActions.js
@@ -4,7 +4,7 @@ module.exports = {
         return queryInterface.createTable('StoredTriggerableActions', {
             id        : { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
             type      : { type: Sequelize.ENUM('ACTIVATE_USER', 'RESET_USER_PASSWORD', 'RESET_ADMIN_PASSWORD'), allowNull: false },
-            data      : { type: Sequelize.JSON, allowNull: false },
+            payload   : { type: Sequelize.JSON, allowNull: false },
             createdAt : { type: Sequelize.DATE, allowNull: false },
             updatedAt : { type: Sequelize.DATE, allowNull: false }
         });
