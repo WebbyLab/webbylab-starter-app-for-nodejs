1a1d4bda3797c660fc140c78684905cd0ba4df29
Vira Rodionova <bepa.rdnv@gmail.com>
2020-04-22 15:20:54 +0300

    Create User even if email is not sent

--- tests/fixtures/use-cases/admin/users-create/positive/02-fail-email/config.mjs
+++ tests/fixtures/use-cases/admin/users-create/positive/02-fail-email/config.mjs
@@ -1,17 +1,15 @@
 import nodemailerMock from 'nodemailer-mock';
 import UsersCreate    from '../../../../../../../lib/use-cases/main/users/Create.mjs';
 
-// export const useCaseClass = UsersCreate;
-// export async function before(factory) {
-//     await factory.standardSetup();
-//     await factory.setupUsers();
-// }
-
 export default {
     useCaseClass : UsersCreate,
     before       : async (factory) => {
         await factory.standardSetup();
         await factory.setupUsers();
         nodemailerMock.mock.setShouldFailOnce();
+
+        const admins = await factory.setupAdmins();
+
+        return admins[0].id;
     }
 };
