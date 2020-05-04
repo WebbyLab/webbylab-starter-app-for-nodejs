8a85c9bd8f27d27cfa1ddeeb67479e2cdb3ff07c
Vira Rodionova <bepa.rdnv@gmail.com>
2020-05-04 11:36:07 +0300

    Setup JsonRpcTester

---
+++ tests/api/json-rpc/main/users-create.test.mjs
@@ -0,0 +1,24 @@
+import { getDirName } from '../../../../lib/utils/index.mjs';
+import Tester         from '../../../lib/JsonRPCTester.mjs';
+
+const tester = new Tester();
+
+const dirname = getDirName(import.meta.url);
+
+tester.setupTestsWithTransactions(`${dirname}/../../../fixtures/use-cases/main/users-create/positive`,
+    'users-create/positive',
+    async ({ config: { useCaseClass, before }, input, expected, checkSideEffects }) => {
+        await before(tester.factory);
+        const result = await tester.testUseCasePositive({ method: useCaseClass.name, input, expected });
+
+        await checkSideEffects({ userId: result.data.id });
+    }
+);
+
+tester.setupTestsWithTransactions(`${dirname}/../../../fixtures/use-cases/main/users-create/negative`,
+    'users-create/negative',
+    async ({ config: { useCaseClass, before }, input, exception }) => {
+        await before(tester.factory);
+        await tester.testUseCaseNegative({ method: useCaseClass.name, input, exception });
+    }
+);
---
+++ tests/lib/JsonRPCTester.mjs
@@ -0,0 +1,73 @@
+import MoleClient        from 'mole-rpc/MoleClient.js';
+import X                 from 'mole-rpc/X.js';
+import WebSocket         from 'ws';
+import TransportClientWS from 'mole-rpc-transport-ws/TransportClientWS.js';
+
+import * as JsonRPC      from '../../lib/api/json-rpc/app.mjs';
+
+import Base from './Base.mjs';
+
+class JsonRPCTester extends Base {
+    #ws = null;
+    #rpcClient = null;
+
+    constructor(...params) {
+        super(...params);
+
+        // TODO: check async init
+        JsonRPC.start({ wssPort: '12345' });
+        this.#ws = new WebSocket('ws://localhost:12345/');
+        this.#rpcClient = new MoleClient({
+            transport : new TransportClientWS({
+                wsBuilder : () => this.#ws
+            })
+        });
+    }
+
+    async testUseCasePositive({ method, input = {}, expected = {} } = {}) {
+        const rpcClient = this.#rpcClient;
+
+        async function useCaseRunner() {
+            const response = await rpcClient.callMethod(method, [ input ]);
+
+            return response;
+        }
+
+        return this._testUseCasePositiveAbstract({
+            useCaseRunner,
+            expected
+        });
+    }
+
+    async testUseCaseNegative({ method, input = {}, exception = {} } = {}) {
+        const rpcClient = this.#rpcClient;
+
+        async function useCaseRunner() {
+            const response = await rpcClient.callMethod(method, [ input ]);
+
+            return response;
+        }
+
+        return this._testUseCaseNegativeAbstract({
+            useCaseRunner,
+            exception
+        });
+    }
+
+    async _testUseCaseNegativeAbstract({ useCaseRunner, exception = {} } = {}, assert = this.testContext) {
+        const error = await assert.throwsAsync(
+            useCaseRunner,
+            { instanceOf: X.ExecutionError }
+        );
+
+        assert.deepEqual(error, new X.ExecutionError({ data: exception }));
+    }
+
+    // #getApiPrefix = () => {
+    //     // global.REST_API_PORT is defined in RestAPI app.
+    //     // TODO: better way is to import app and use server.address() to get the port
+    //     return  `ws://localhost:${global.REST_API_PORT}`;
+    // }
+}
+
+export default JsonRPCTester;
